60e4eaec0cb8551068686c409f02b4b2fca05cc8
Anup Kumar <anuprulez@gmail.com>
2025-05-08 12:24:00 +0200

    update

--- app.py
+++ app.py
@@ -21,14 +21,21 @@ async def main():
     agent = MCPAgent(llm=llm, client=client, max_steps=30)
 
     # Run the query
+    '''result = await agent.run(
+        "Please perform the following tasks in sequence:\n"
+        "1. Create a new Galaxy history and name it 'MCP_History_new'.\n"
+        "2. Upload the training dataset located at '/home/anup/Downloads/classification_local_train_rows.tsv' to the 'MCP_History_new'. After uploading, return the dataset ID as 'train_id'.\n"
+        "3. Upload the testing dataset located at '/home/anup/Downloads/classification_local_test_rows.tsv' to the same 'MCP_History_new'. After uploading, return the dataset ID as 'test_id'.\n"
+        "4. Use the tool with ID 'tabpfn' in its latest version. Run it in the 'MCP_History_new' using 'train_id' and 'test_id' as the inputs for training and testing, respectively. Return the job output ID."
+    )'''
+
     result = await agent.run(
-        "Perform these tasks: \
-         1. Create a history with name 'MCP_History'. \
-         2. Use the above history 'MCP_History' and upload train dataset from '/home/anup/Downloads/classification_local_train_rows.tabular' and return dataset id \
-         3. Use the above history 'MCP_History' and upload test data from '/home/anup/Downloads/classification_local_test_rows.tabular' and return dataset id"
-         #3. Use train and test dataset IDs from the previous step and run the latest version of the TabPFN (tool_id tabpfn) tool on the uploaded train and test datasets in 'MCP_history' history ."
+        "Fetch the logged in user information from the MCP server. Return the user ID and the user name."
     )
+
     print(f"\nResult: {result}")
+
+
 
 if __name__ == "__main__":
     asyncio.run(main())
